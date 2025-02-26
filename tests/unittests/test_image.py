fb7a11b309ab548fced0f3bf2dbd85e04fd3a60c
yaya <yayatang@users.noreply.github.com>
2025-02-26 15:04:52 +0200

    Apps 1689 embedder (#2)
    - change to compute config 
    - fix unittests

--- tests/unittests/test_image.py
+++ tests/unittests/test_image.py
@@ -1,10 +1,8 @@
 import unittest
-import dtlpy as dl
 import os
-print(os.getcwd())
 
-from adapter import DINOv2Adapter
 from PIL import Image
+from adapter import DINOv2Adapter
 
 
 class TestModelAdapter(unittest.TestCase):
@@ -12,9 +10,9 @@ class TestModelAdapter(unittest.TestCase):
     def test_embed_image(self):
         adapter = DINOv2Adapter()
         adapter.load(local_path="")
-        image = Image.open(os.path.join("assets", "image.jpg"))
+        image = Image.open(os.path.join(os.path.abspath("../../tests/assets/unittests/image.png")))
         features = adapter.embed_images([image])
-        print(features)
+        assert len(features[0]) == 384
 
 
 if __name__ == "__main__":
